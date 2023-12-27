1bc9241088b65a124abcdecc6a5baa8d4e2a28df
tsirulik <146517372+tsirulik@users.noreply.github.com>
2023-12-27 11:41:30 +0500

    Delete module2.py

--- module2.py
+++
@@ -1,9 +0,0 @@
-from functools import lru_cache
-@lru_cache(maxsize = 2)
-def fib(n):
-    if n<=2:
-        return 1
-    return fib (n-1)+fib(n-2)
-if __name__=='__main__':
-    for i in range(1,11):
-        print(i,fib(i))
