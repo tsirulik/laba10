6136ef40b9299ba1e6d22eb4bf567fb6358204e8
tsirulik <146517372+tsirulik@users.noreply.github.com>
2023-12-27 11:41:40 +0500

    Delete module3.py

--- module3.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-def generate_password(length=8):
-    characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for _ in range(length))
-    yield password
-password = next(generate_password(8))
-print(password)
